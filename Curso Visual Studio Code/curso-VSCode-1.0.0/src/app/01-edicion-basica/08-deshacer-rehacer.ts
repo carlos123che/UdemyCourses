a1ad02b7654cb59f3cae3854082df544acfcd063
Carlos che <car123che@gmail.com>
2021-04-11 18:36:17 -0600

    Curso css de 0 a experto

---
+++ Curso Visual Studio Code/curso-VSCode-1.0.0/src/app/01-edicion-basica/08-deshacer-rehacer.ts
@@ -0,0 +1,27 @@
+/*
+    Objetivo:
+        Deshacer y rehacer el código
+§
+    Tips:
+        ⌘ Z
+        ⌘ ⇧ Z
+        Ctrl +Z
+        Ctrl + Shift + Z
+
+*/
+
+/* jf
+ajaja
+ja
+ja */
+
+
+
+
+// jfajja jajaja jajaj jajaj ajaj
+// Demo
+// function holaMundo() {
+//     return 'Saludos a todos!';
+// }
+
+
